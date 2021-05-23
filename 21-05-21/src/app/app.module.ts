f3481c69e9d7d367975d70cbc85af2328acb9266
Anitha <anithamedikonda19@gmail.com>
2021-05-23 15:54:54 +0530

    angular-activity

---
+++ 21-05-21/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+//import { AppComponent } from './app.component';
+//import { GameComponent } from './game/game.component';
+import { EvenComponent } from './even/even.component';
+import { GameComponent } from './game-control/game-control.component';
+import { OddComponent } from './odd/odd.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GameComponent,
+    EvenComponent,
+    OddComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
